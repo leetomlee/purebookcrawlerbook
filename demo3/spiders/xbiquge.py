a7ce372359bffbadbca860429dee97a2032b7b7d
lichao <leetomlee123@gamil.com>
2021-05-04 18:02:37 +0800

    1111

--- demo3/spiders/xbiquge.py
+++ demo3/spiders/xbiquge.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+import logging
 
 import pymongo
 from elasticsearch import Elasticsearch
-from redis import StrictRedis
 from scrapy import Selector
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -18,23 +18,33 @@ s = 0
 # db = client.admin
 #
 # db.authenticate('lx', 'Lx123456')
-#myclient = pymongo.MongoClient('mongodb://lx:Lx123456@localhost:27017/', connect=False)
+# myclient = pymongo.MongoClient('mongodb://lx:Lx123456@localhost:27017/', connect=False)
 myclient = pymongo.MongoClient('mongodb://lx:Lx123456@134.175.83.19:27017/')
 mydb = myclient["book"]
 bookDB = mydb["books"]
 chapterDB = mydb["chapters"]
 es = Elasticsearch(
-     ['134.175.83.19:8088'],
-     # 认证信息
-     http_auth=('elastic', 'Z2jJ2sZWGPy0bulSf4dZ')
- )
-#es = Elasticsearch(
+    ['134.175.83.19:8088'],
+    # 认证信息
+    http_auth=('elastic', 'Z2jJ2sZWGPy0bulSf4dZ')
+)
+# es = Elasticsearch(
 #    ['localhost:8088'],
 # 
 #    http_auth=('elastic', 'Z2jJ2sZWGPy0bulSf4dZ')
-#)
-redis = StrictRedis(host='134.175.83.19', port=6379, db=1, password='zx222lx')
-#redis = StrictRedis(host='localhost', port=6379, db=1, password='zx222lx')
+# )
+# redis = StrictRedis(host='134.175.83.19', port=6379, db=1, password='zx222lx')
+# redis = StrictRedis(host='localhost', port=6379, db=1, password='zx222lx')
+keys = []
+for book in bookDB.find({}, {"book_name": 1, "author": 1}):
+    try:
+
+        key = book['book_name'] + book['author']
+        keys.append(key)
+    except Exception as e:
+        logging.error(e)
+        continue
+logging.info("去重关键字:"+str(len(keys)))
 
 
 class MeiziSpider(CrawlSpider):
@@ -58,15 +68,10 @@ class MeiziSpider(CrawlSpider):
         selector = Selector(response=response)
         book_name = selector.xpath("//meta[@property='og:novel:book_name']/@content").extract_first()
         author = selector.xpath("//meta[@property='og:novel:author']/@content").extract_first()
-        key = book_name + author
-        if redis.exists(key):
+        key1 = book_name + author
+        if keys.__contains__(key1):
             return
-            # one = bookDB.find_one({'book_name': book_name, 'author': author})
-            # bookDB.delete_one({'book_name': book_name, 'author': author})
-            # book_id = str(one['_id'])
-            # chapterDB.delete_many({'book_id': book_id})
-
-        redis.set(key, '')
+        keys.append(key1)
         cover = selector.xpath("//meta[@property='og:image']/@content").extract_first()
         category = selector.xpath("//meta[@property='og:novel:category']/@content").extract_first()
         status = selector.xpath("//meta[@property='og:novel:status']/@content").extract_first()
@@ -89,7 +94,7 @@ class MeiziSpider(CrawlSpider):
         book.add_value('last_chapter', latest_chapter_name)
         item = book.load_item()
         book_id = str(bookDB.insert_one(dict(item)).inserted_id)
-        index = es.index(index="book",  id=book_id,
+        index = es.index(index="book", id=book_id,
                          body={"book_name": book_name, "book_author": author})
         chapters = []
         for dd in selector.xpath("//*[@id='list']/dl/dd"):
@@ -103,3 +108,6 @@ class MeiziSpider(CrawlSpider):
                 chapters.append(chapter)
         if len(chapters) > 0:
             chapterDB.insert_many(chapters)
+
+
+
